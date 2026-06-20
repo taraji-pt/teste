09fe0d62284f1ebb5ac11eceeb4e114a6e796fa3
taraji-pt <tterraquente@gmail.com>
2026-06-20 23:50:11 +0100

    Update ipma.py

--- scripts/ipma.py
+++ scripts/ipma.py
@@ -1,1 +1,11 @@
-print("Teste OK")
+import requests
+
+URL = "https://api.ipma.pt/open-data/observation/meteorology/stations/observations.json"
+
+r = requests.get(URL, timeout=30)
+r.raise_for_status()
+
+dados = r.json()
+
+print(type(dados))
+print("Total registos:", len(dados))
